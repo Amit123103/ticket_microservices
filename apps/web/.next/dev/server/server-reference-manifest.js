c30b50891e029fb9cb57630535c345e71c5b1b8e
Amit123103 <amitakhil001@gmail.com>
2026-08-06 10:12:45 +0530

    fix: remove .next build directory from git tracking and add to .gitignore

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nZ+W+me8vwer5BiQl7xGT2bLDBJTYJ+0f43oKBO9oTI=\"\n}"
--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xEr/9V0k3BGs5MLGlOdvjQGzciLC1bOOjVYkwkWvqOY=\"\n}"
